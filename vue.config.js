9b3273f40fa1070d8dbb6854574cc113a9898154
javawg@hotmail.com <javawg@hotmail.com>
2021-08-25 08:43:16 +0800

    u

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 console.log('BASE_API: ' + process.env.VUE_APP_BASE_API)
 console.log('OUTPUT_DIR: ' + process.env.VUE_APP_OUTPUT_DIR)
 module.exports = {
-  publicPath: "/vue-static-client",
+  publicPath: "/client",
   outputDir: process.env.VUE_APP_OUTPUT_DIR,
   assetsDir: "./aassets",
   devServer: {
